1cb236853eb3711af5dc7a505284dfa28d2c16b4
mjtedder <mjtedder@gmail.com>
2018-08-04 12:01:17 -0400

    Added comment in order to be displayed as a contributer

--- routes/api/ads.js
+++ routes/api/ads.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const adsController = require('../../controllers/adsController')
 
+// FINISH THIS BY SATURDAY
 //api routes will go here
 router.route('/')
 
