20839b928f2859d97e8b65fa4e81db34311741bc
mjtedder <mjtedder@gmail.com>
2018-08-08 10:45:13 -0400

    campaign/ad controllers added, routes for both added

---
+++ controllers/campaignController.js
@@ -0,0 +1,38 @@
+const Campaign = require('../models');
+
+// CRUD methods for campaignController
+module.exports = {
+  // Get all campaigns
+  findAll: function(req, res) {
+    Campaign.find(req.query)
+    // Sort campaigns by ending soon to just posted
+    .sort({ endDate: -1})
+    .then(campaign => res.json(campaign))
+    .catch(err => res.status(422).json(err))
+  },
+  // Get a specific campaign
+  findById: function(req, res) {
+    Campaign.findById(req.params.id)
+    .then(campaign => res.json(campaign))
+    .catch(err => res.status(422).json(err))
+  },
+  // Business creates a campaign
+  create: function(req, res) {
+    Campaign.create(req.body)
+    .then(campaign => res.json(campaign))
+    .catch(err => res.status(422).json(err))
+  },
+  // Update a campaign
+  update: function(req, res) {
+    Campaign.findOneAndUpdate({ _id: req.params.id }, req.body)
+    .then(campaign => res.json(campaign))
+    .catch(err => res.status(422).json(err))
+  },
+  // Delete a campaign
+  remove: function(req, res) {
+    Campaign.findById({ _id: req.params.id })
+    .then(campaign => campaign.remove())
+    .then(campaign => res.json(campaign))
+    .catch(err => res.status(422).json(err))
+  }
+}
--- routes/api/ads.js
+++ routes/api/ads.js
@@ -1,11 +1,23 @@
 const router = require('express').Router()
 const adsController = require('../../controllers/adsController')
 
-// FINISH THIS BY SATURDAY
-//api routes will go here
+// Matches with "/api/ads"
+// Find all ads
+// Business can create an ad
+
 router.route('/')
  .get(adsController.findAll)
  .post(adsController.create)
 
+// Matches with "/api/ads/:id"
+// Finds a specific ad
+// Business can update an ad
+// Users also update the clicks field on an ad?
+// Business can delete an ad
+
+router.route('/:id')
+  .get(adsController.findById)
+  .put(adsController.update)
+  .delete(adsController.remove)
 
 module.exports = router
