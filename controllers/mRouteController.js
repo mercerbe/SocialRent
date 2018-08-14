ad41a4586579e1a72237f23a87c7457484674cfa
RomeoKilo125 <romeokilo125@gmail.com>
2018-08-14 19:46:43 -0400

    mroutes redirecting to destination! $$$$

--- controllers/mRouteController.js
+++ controllers/mRouteController.js
@@ -5,6 +5,6 @@ module.exports = {
   // add 1 to the clicks, then redirects the browser to the url for the same ad
   mRoute: function(req, res) {
     console.log(`routing: ${req.params.mRoute}`)
-    Ad.findOneAndUpdate({ mRoute: req.params.mRoute}, {$inc: {clicks: 1}}, {new: true}).then(dbAd => res.json(dbAd.url))
+    Ad.findOneAndUpdate({ mRoute: req.params.mRoute}, {$inc: {clicks: 1}}, {new: true}).then(dbAd => res.redirect(dbAd.url))
   }
 }
