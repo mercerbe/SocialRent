858f49cb236b567eadae49224e6641f6c37c49d9
RomeoKilo125 <romeokilo125@gmail.com>
2018-08-14 19:44:47 -0400

    mroutes successfully clicking and passing back a url

--- controllers/mRouteController.js
+++ controllers/mRouteController.js
@@ -4,6 +4,7 @@ module.exports = {
   // mRoute will find the ad with that mRoute,
   // add 1 to the clicks, then redirects the browser to the url for the same ad
   mRoute: function(req, res) {
-    Ad.findOneAndUpdate({_id: req.params.mRoute}, {$inc: {clicks: 1}}, {new: true}).then(dbAd => res.redirect(dbAd.url))
+    console.log(`routing: ${req.params.mRoute}`)
+    Ad.findOneAndUpdate({ mRoute: req.params.mRoute}, {$inc: {clicks: 1}}, {new: true}).then(dbAd => res.json(dbAd.url))
   }
 }
