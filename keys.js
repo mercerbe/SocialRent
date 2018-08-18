9df9bf8a71a99f450218725049c2c4fed021b065
mjtedder <mjtedder@gmail.com>
2018-08-18 13:13:55 -0400

    push changes for merge into master

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('Keys Loaded');
+console.log(process.env.TWITTER_ACCESS_TOKEN_KEY)
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
